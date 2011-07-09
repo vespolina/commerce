48e3d2df64084c3182294be2f3adc52abb5dafc6
IamPersistent <develop@zestic.com>
2011-07-09 09:21:56 -0700

    setters and getters in product

--- Model/Product.php
+++ Model/Product.php
@@ -2,36 +2,139 @@
 /**
  * (c) Vespolina Project http://www.vespolina-project.org
  *
- * (c) Daniel Kucharski <daniel@xerias.be>
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
 
 namespace Vespolina\ProductBundle\Model;
 
-use Vespolina\PricingBundle\Model\PricingElementContainer;
-use Vespolina\PricingBundle\Model\PricingSetInterface;
+use Vespolina\ProductBundle\Model\ProductNodeInterface;
+use Vespolina\ProductBundle\Model\Node\FeatureInterface;
+use Vespolina\ProductBundle\Model\Node\ProductFeaturesInterface;
+use Vespolina\ProductBundle\Model\Node\ProductIdentifiersInterface;
 
+/**
+ * @author Richard D Shank <develop@zestic.com>
+ */
 class Product implements ProductInterface
 {
-    protected $pricingSet = array();
+    protected $features;
+    protected $description;
+    protected $id;
+    protected $identifiers;
+    protected $name;
+    protected $options;
+    protected $type;
 
+    /**
+     * @inheritdoc
+     */
     public function getId()
     {
+        return $this->id;
     }
 
-    public function getPricingSets()
+    /**
+     * @inheritdoc
+     */
+    public function setDescription($description)
     {
-        return $this->pricingSets;
+        $this->description = $description;
     }
 
-    public function setPricingSets($pricingSets)
+    /**
+     * @inheritdoc
+     */
+    public function getDescription()
     {
-        return $this->pricingSets = $pricingSets;
+        return $this->description;
     }
 
-    public function addPricingSet(PricingSetInterface $pricingSet)
+    /**
+     * @inheritdoc
+     */
+    public function addFeature(FeatureInterface $feature)
     {
-        $this->pricingSets[$pricingSet->getKey()] = $pricingSet;
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setFeatures($features)
+    {
+        $this->features = $features;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFeatures()
+    {
+        return $this->features;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setIdentifiers(ProductIdentifiersInterface $identifiers)
+    {
+        $this->identifiers = $identifiers;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getIdentifiers()
+    {
+        return $this->identifiers;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getType()
+    {
+        return $this->type;
     }
 }
