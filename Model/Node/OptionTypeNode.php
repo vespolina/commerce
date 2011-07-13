39e8638fc634c58851330a555fc55007d9d55274
IamPersistent <develop@zestic.com>
2011-07-12 21:25:01 -0700

    create OptionNodeType class

---
+++ Model/Node/OptionTypeNode.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * (c) Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\ProductBundle\Model\Node;
+
+use Vespolina\ProductBundle\Model\ProductNode;
+use Vespolina\ProductBundle\Model\Node\OptionTypeNodeInterface;
+
+/**
+ * @author Richard D Shank <develop@zestic.com>
+ */
+class OptionTypeNode extends ProductNode implements OptionTypeNodeInterface
+{
+    /*
+     * @inheritdoc
+     */
+    public function addOption(OptionNodeInterface $option)
+    {
+        $this->addChild($option);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function clearOptions()
+    {
+        $this->clearChildren();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setOptions($options)
+    {
+        $this->setChildren($options);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function removeOption(OptionNodeInterface $option)
+    {
+        $this->removeChild($option->getName());
+    }
+}
---
+++ Model/Node/OptionTypeNodeInterface.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * (c) Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\ProductBundle\Model\Node;
+
+use Vespolina\ProductBundle\Model\ProductNodeInterface;
+
+/**
+ * @author Richard D Shank <develop@zestic.com>
+ */
+interface OptionTypeNodeInterface extends ProductNodeInterface
+{
+    /**
+     * Add a option to this product options node.
+     *
+     * @param Vespolina\ProductBundle\Model\Node\OptionNodeInterface $option
+     */
+    public function addOption(OptionNodeInterface $option);
+
+    /**
+     * Clear all options from this product options
+     */
+    public function clearOptions();
+
+    /**
+     * Add a collection of options
+     *
+     * @param array $options
+     */
+    public function setOptions($options);
+
+    /**
+     * Remove a option from this product options set
+     *
+     * @param OptionNodeInterface $option
+     */
+    public function removeOption(OptionNodeInterface $option);
+}
