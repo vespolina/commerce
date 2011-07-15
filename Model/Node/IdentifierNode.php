f280005266243738fe27967c6ed3f5eaa1e0c59b
IamPersistent <develop@zestic.com>
2011-07-15 01:01:22 -0700

    fix typo in namespace

--- Model/Node/IdentifierNode.php
+++ Model/Node/IdentifierNode.php
@@ -5,15 +5,15 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
-namespace Vesoplina\ProductBundle\Model\Node;
+namespace Vespolina\ProductBundle\Model\Node;
 
 use Vespolina\ProductBundle\Model\ProductNode;
-use Vesoplina\ProductBundle\Model\Node\IdentifierNodeInterface;
+use Vespolina\ProductBundle\Model\Node\IdentifierNodeInterface;
 
 /**
  * @author Richard D Shank <develop@zestic.com>
  */
-class IdentifierNode extends ProductNode
+class IdentifierNode extends ProductNode implements IdentifierNodeInterface
 {
     protected $code;
 
