0ac9f7d735a3eb8c61592f929249caf4c7cd93c4
IamPersistent <develop@zestic.com>
2011-07-14 23:19:01 -0700

    update product test to handle identifiers

--- Tests/Model/ProductTest.php
+++ Tests/Model/ProductTest.php
@@ -11,6 +11,8 @@ namespace Vespolina\ProductBundle\Tests\Model;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 use Vespolina\ProductBundle\Model\Product;
+use Vespolina\ProductBundle\Model\Node\ProductIdentifiers;
+use Vespolina\ProductBundle\Model\Node\IdentifierNode;
 
 /**
  * @author Richard D Shank <develop@zestic.com>
@@ -25,13 +27,36 @@ class ProductTest extends WebTestCase
             $product->getFeatures(),
             'an empty class with ProductFeaturesInterface should be set');
         $this->assertInstanceOf(
-            'Vespolina\ProductBundle\Model\Node\ProductIdentifiersInterface',
-            $product->getIdentifiers(),
-            'an empty class with ProductIdentifiersInterface should be set');
-        $this->assertInstanceOf(
             'Vespolina\ProductBundle\Model\Node\ProductOptionsInterface',
             $product->getOptions(),
             'an empty class with ProductOptionsInterface should be set');
-        
+
+        $product->setPrimaryIdentifer('TestNode');
+        $this->assertSame('TestNode', $product->getPrimaryIdentifer(), 'the identifier node can be set by string');
+
+        $product->setPrimaryIdentifier(new IdentifierNode());
+        $this->assertSame(
+            'Vespolina\ProductBundle\Model\Node\IdentifierNode',
+            $product->getPrimaryIdentifer(),
+            'the identifier node can be set by instance'
+        );
+
+        $testSKU = $this->getMock('Vespolina\ProductBundle\Model\Node\IdentifierNodeId', array('getCode'));
+        $testSKU->expects($this->any())
+                 ->method('getCode')
+                 ->will($this->returnValue('AB-CD-EF-GH'));
+
+        $product->setPrimaryIdentifier($testSKU);
+        $pi = new ProductIdentifiers();
+        $pi->addIdentifier($testSKU);
+
+        $productIdentifiers = new \ReflectionProperty('Vespolina\ProductBundle\Model\Product', 'productIdentifiers');
+        $productIdentifiers->setAccessible(true);
+
+        $this->assertArrayHasKey(
+            'AB-CD-EF-GH',
+            $productIdentifiers->getValue($product),
+            'ProductIdentifiers should be indexed by the principle identifier type value'
+        );
     }
 }
