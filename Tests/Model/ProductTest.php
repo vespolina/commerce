e319d3dd3f6fd35f17392673282dc1eff2ea3890
IamPersistent <develop@zestic.com>
2011-07-29 09:57:13 -0700

    add tests for identifierSets in product

--- Tests/Model/ProductTest.php
+++ Tests/Model/ProductTest.php
@@ -19,11 +19,10 @@ use Vespolina\ProductBundle\Model\Node\IdentifierNode;
  */
 class ProductTest extends WebTestCase
 {
-    public function testProduct()
+    public function testProductOptions()
     {
         $product = new Product();
 
-        /* options */
         $this->assertInstanceOf(
             'Vespolina\ProductBundle\Model\Node\ProductOptionsInterface',
             $product->getOptions(),
@@ -42,8 +41,12 @@ class ProductTest extends WebTestCase
             $product->getOptions()->getOption('size', 'large'),
             'addOption hands option off to ProductOptions'
         );
+    }
 
-        /* product features */
+    public function testProductFeatures()
+    {
+        $product = new Product();
+
         $productFeatures = new \ReflectionProperty('Vespolina\ProductBundle\Model\Product', 'features');
         $productFeatures->setAccessible(true);
 
@@ -60,6 +63,62 @@ class ProductTest extends WebTestCase
         $this->assertArrayHasKey('label', $features, 'top level key is the type in lower case');
         $this->assertArrayHasKey('joat music', $features['label'], 'top level key is the search term in lower case');
 
+    }
 
+    public function testProductIdentities()
+    {
+        $product = new Product();
+
+        $identifierSet = $this->createProductIdentifierSet('test123');
+
+        $product->addIdentifierSet('test123', $identifierSet);
+        $this->assertInstanceOf(
+            'Doctrine\Common\Collections\ArrayCollection',
+            $product->getIdentifiers(),
+            'the identifiers should be stored in an ArrayCollection'
+        );
+
+        $this->assertSame(
+            $identifierSet,
+            $product->getIdentifier('test123'),
+            'the identifier should be returned by the key'
+        );
+
+        $product = new Product();
+
+        $identifierSet = $this->createProductIdentifierSet('test123');
+
+        $product->addIdentifierSet('test123', $identifierSet);
+
+        $identifiers = array();
+
+        $identifier['abc'] = $this->createProductIdentifierSet('abc');
+        $identifier['123'] = $this->createProductIdentifierSet('123');
+        
+        $product->setIdentifiers($identifiers);
+        $this->assertInstanceOf(
+            'Doctrine\Common\Collection\ArrayCollection',
+            $product->getIdentifiers(),
+            'an array of IdentifierSets should be put into an ArrayCollection'
+        );
+        $this->assertEquals(
+            2,
+            $product->getIdentifiers()->count(),
+            'any identifier sets already in product are removed when setIdentifiers is called'
+        );
+    }
+
+    protected function createProductIdentifierSet($code)
+    {
+        $pi = new ProductIdentifierSet();
+        $identifier = $this->getMock('Vespolina\ProductBundle\Model\Node\IdentifierNode', array('getCode', 'getName'));
+        $identifier->expects($this->any())
+             ->method('getCode')
+             ->will($this->returnValue($code));
+        $identifier->expects($this->any())
+             ->method('getName')
+             ->will($this->returnValue($code));
+        $pi->addIdentifier($identifier);
+        return $pi;
     }
 }
