67b9bec8fd166c30d57a7741f456b8264e136732
IamPersistent <develop@zestic.com>
2011-08-05 22:30:35 -0700

    update to handle abstract class

--- Tests/Model/Node/OptionGroupTest.php
+++ Tests/Model/Node/OptionGroupTest.php
@@ -30,7 +30,7 @@ class OptionGroupTest extends WebTestCase
         $sizeXl->setType('size');
         $sizeXl->setValue('extra large');
 
-        $otn = new OptionGroup();
+        $otn = $this->createOptionGroup();
         // DO NOT SET THE NAME!
         $otn->addOption($colorRed);
 
@@ -48,5 +48,11 @@ class OptionGroupTest extends WebTestCase
 
         $this->setExpectedException('UnexpectedValueException', 'All OptionsNodes in this type must be color');
         $otn->addOption($sizeXl);
+    }
+
+    protected function createOptionGroup()
+    {
+        $og = $this->getMockForAbstractClass('Vespolina\ProductBundle\Model\Node\OptionGroup');
+        return $og;
     }
 }
