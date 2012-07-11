398678877bf7df0196d01df96fd95a9e35b70731
iampersistent <develop@zestic.com>
2012-07-11 00:34:03 -0700

    update composer.json and testing so it takes advantage of composer

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,14 +1,10 @@
 <?php
 
-/*
- * This file is part of the VespolinaCartBundle package.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-if (file_exists($file = __DIR__.'/autoload.php')) {
-    require_once $file;
-} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
-    require_once $file;
+if (!@include __DIR__ . '/../vendor/autoload.php') {
+    die(<<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
 }
