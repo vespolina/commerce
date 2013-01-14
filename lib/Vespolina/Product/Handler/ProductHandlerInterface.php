82e99e63212173d6956da565dca779303905893b
iampersistent <develop@zestic.com>
2013-01-14 06:30:02 -0800

    revert back to namespace in docs

--- lib/Vespolina/Product/Handler/ProductHandlerInterface.php
+++ lib/Vespolina/Product/Handler/ProductHandlerInterface.php
@@ -1,20 +1,18 @@
 <?php
 /**
- * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ * (c) 2012-2013 Vespolina Project http://www.vespolina-project.org
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
 namespace Vespolina\Product\Handler;
 
-use Vespolina\Entity\Product\ProductInterface;
-
 interface ProductHandlerInterface
 {
     /**
      * Return a newly created product type for the handler
      *
-     * @return ProductInterface
+     * @return \Vespolina\Entity\Product\ProductInterface
      */
     function createProduct();
 
