7985b5aa21a5dd931d1844636c0f9d174806b03b
inspiran <daniel@xerias.be>
2013-04-18 23:55:49 +0200

    Added a pricing manager

---
+++ lib/Vespolina/Pricing/Manager/PricingManagerInterface.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * (c) Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\Pricing\Manager;
+
+use Vespolina\Entity\Pricing\PricingSetInterface;
+
+
+/**
+ * The pricing manager is responsible for instatiating pricing sets,
+ * registering pricing set definitions and so on...
+ *
+ * @author Daniel Kucharski <daniel@xerias.be>
+ */
+interface PricingManagerInterface
+{
+
+    /**
+     * Create a pricing set by it's configuration name
+     *
+     * @param $name
+     * @return Vespolina\Entity\Pricing\PricingSetInterface
+     */
+    function createPricingSet($name = 'default', array $pricingValues = array());
+
+    /**
+     * Add a pricing set configuration
+     *
+     * @param $name
+     * @param $pricingSetClass
+     * @param array $pricingElementClasses
+     * @return mixed
+     */
+    function addConfiguration($name, $pricingSetClass, array $pricingElementClasses = array());
+
+}
